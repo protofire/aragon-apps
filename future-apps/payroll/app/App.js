2bd813c9212dbfb88908e73f80fbf216a80d253d
Sebastián Galiano <sgaliano@gmail.com>
2018-09-19 10:16:38 -0300

    Tabbed UI layout

--- future-apps/payroll/app/App.js
+++ future-apps/payroll/app/App.js
@@ -1,28 +1,108 @@
 import React from 'react'
-import styled from 'styled-components'
-import { AragonApp, AppBar } from '@aragon/ui'
+import styled, { css } from 'styled-components'
+import { AragonApp, AppBar, Button } from '@aragon/ui'
 
-import AppLayout from './components/AppLayout'
+import Tab from './components/Tab'
+
+import { MyPayroll, TeamPayroll } from './components/sections'
 
 const AppContainer = styled(AragonApp)`
   display: flex;
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: stretch;
+`
+
+const Header = styled.div`
+  flex-shrink: 0;
+`
+
+const ScrollPane = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  overflow: auto;
+  flex-grow: 1;
+
+  ${({ vertical = true }) => vertical && css`
+    overflow-y: auto;
+  `}
+  
+  ${({ horizontal = true }) => horizontal && css`
+    overflow-x: auto;
+  `}
+`
+
+const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  flex-grow: 1;
 `
 
 export default class App extends React.Component {
+  state = { activeTab: 'my-payroll' }
+
+  handleTabChange = (index, name) => {
+    this.setState({ activeTab: name })
+  }
+
+  renderActionButton () {
+    if (this.state.activeTab === 'my-payroll') {
+      return (
+        <Button mode='strong'>
+          Request salary
+        </Button>
+      )
+    } else if (this.state.activeTab === 'team-payroll') {
+      return (
+        <Button mode='strong'>
+          Add new employee
+        </Button>
+      )
+    }
+
+    return null
+  }
+
   render () {
+    const { activeTab } = this.state
+
     return (
       <AppContainer publicUrl='/aragon-ui/'>
-        <AppLayout>
-          <AppLayout.Header>
-            <AppBar title='Payroll'/>
-          </AppLayout.Header>
+        <Header>
+          <AppBar
+            title='Payroll'
+            endContent={this.renderActionButton()}
+          />
 
-          <AppLayout.ScrollWrapper>
-            <AppLayout.Content>
-              {/* */}
-            </AppLayout.Content>
-          </AppLayout.ScrollWrapper>
-        </AppLayout>
+          <Tab.Container onTabChange={this.handleTabChange}>
+            <Tab
+              name='my-payroll'
+              title='My payroll'
+            />
+
+            <Tab
+              name='team-payroll'
+              title='Team payroll'
+            />
+          </Tab.Container>
+
+        </Header>
+
+        <ScrollPane>
+          <Content>
+            {activeTab === 'my-payroll' && (
+              <MyPayroll/>
+            )}
+
+            {activeTab === 'team-payroll' && (
+              <TeamPayroll/>
+            )}
+          </Content>
+        </ScrollPane>
       </AppContainer>
     )
   }
---
+++ future-apps/payroll/app/components/sections/MyPayroll.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const MyPayroll = () => (
+  <Container>
+    My Payroll
+  </Container>
+)
+
+export default MyPayroll
