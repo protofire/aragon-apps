cb7db1adf11e015e49db04fc95c4e21826cef41b
Lisandro Corbalan <lmcorbalan@gmail.com>
2018-10-10 11:13:02 -0300

    Add local kit

---
+++ future-apps/payroll/contracts/PPFMock.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.24;
+
+import "@aragon/ppf-contracts/contracts/IFeed.sol";
+
+
+contract PPFMock is IFeed {
+  function get(address base, address quote) external view returns (uint128 xrt, uint64 when) {
+      xrt = 1;
+      when = uint64(now);
+  }
+}
